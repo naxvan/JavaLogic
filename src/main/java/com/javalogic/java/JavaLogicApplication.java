20aadc68c0a08e4033192e4b045dfdd0ad97f51b
naxvan <mahechalozada@gmail.com>
2024-06-11 11:06:07 -0500

    Problemas

---
+++ src/main/java/com/javalogic/java/JavaLogicApplication.java
@@ -0,0 +1,13 @@
+package com.javalogic.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLogicApplication.class, args);
+    }
+
+}
