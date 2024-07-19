ce6d9fe88ce517496bac15d97010abd4fad13654
naxvan <mahechalozada@gmail.com>
2024-07-19 09:45:46 -0500

    solucion problema1

--- src/main/java/com/javalogic/java/JavaLogicApplication.java
+++ src/main/java/com/javalogic/java/JavaLogicApplication.java
@@ -10,7 +10,6 @@ public class JavaLogicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaLogicApplication.class, args);
-
         Problem1 problem1 = new Problem1();
         ArrayList<Integer> list = problem1.printRange(1, 255);
         System.out.println(list);
